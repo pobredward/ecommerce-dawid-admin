024f1b643066c37ba2e2e2f2b6274c7a6aed6e45
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-06-08 05:23:43 +0000

    Update

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -6,8 +6,8 @@ import clientPromise from "../../../lib/mongodb";
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
     adapter: MongoDBAdapter(clientPromise),
