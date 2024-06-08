7da1d85dfe7aa11eca5a412f04581fdeb5610843
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-06-08 23:35:45 +0000

    Update [...nextauth].tsx

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,4 +1,4 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
@@ -10,7 +10,7 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET || "YOUR_DEFAULT_SECRET",
     adapter: MongoDBAdapter(clientPromise),
 });
 
