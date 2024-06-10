956affe4635982c3fc1ffbf7002daac329f5dfbd
Edward Shin <pobredward@gmail.com>
2024-06-10 05:43:47 +0000

    Update products.ts

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -4,6 +4,11 @@ import mongooseConnect from "../../lib/mongoose";
 const handle = async (req, res) => {
   const { method } = req;
   await mongooseConnect();
+
+  if (method === "GET") {
+    res.json(await Product.find());
+  }
+
   if (method === "POST") {
     const { title, description, price } = req.body;
     const productDoc = await Product.create({
