2538ce6cafb43865c8f1a3820ec16a9390716a95
Edward Shin <pobredward@gmail.com>
2024-06-10 02:24:12 +0000

    Update products.ts

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
-import clientPromise from "../../lib/mongodb";
 import Product from "../../models/Product";
 
 const handle = async (req, res) => {
   const { method } = req;
-  // mongoose.connect(process.env.MONGODB_URI);
-  mongoose.Promise = clientPromise;
+  mongoose.connect(process.env.MONGODB_URI);
   if (method === "POST") {
     const { title, description, price } = req.body;
     const productDoc = await Product.create({
