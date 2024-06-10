34da0c1c7fbf1d8b89c60c32daa56b305e9033d4
Edward Shin <pobredward@gmail.com>
2024-06-10 03:04:05 +0000

    Update new.tsx

--- pages/products/new.tsx
+++ pages/products/new.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import axios from "axios";
+import { redirect } from "next/navigation";
+import { useRouter } from "next/router";
 
 const NewProduct: React.FC = () => {
   const [title, setTitle] = React.useState("");
   const [description, setDescription] = React.useState("");
   const [price, setPrice] = React.useState("");
+  const [goToProducts, setGoToProducts] = React.useState(false);
+  const router = useRouter();
 
   const createProduct = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const data = { title, description, price };
     await axios.post("/api/products", data);
+    setGoToProducts(true);
+  };
+
+  if (goToProducts) {
+    router.push("/products");
   }
 
   return (
