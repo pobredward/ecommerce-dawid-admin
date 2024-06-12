b31d53fc2e81e76693ba5e47358a68a333e320f4
Edward Shin <pobredward@gmail.com>
2024-06-12 03:59:36 +0000

    Create upload.ts

---
+++ pages/api/upload.ts
@@ -0,0 +1,17 @@
+import multiparty from "multiparty";
+
+const handle = async (req, res) => {
+  const form = multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err) throw err;
+    console.log('length:', files.length);
+    console.log(fields);
+    res.json("ok");
+  });
+};
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+export default handle;
