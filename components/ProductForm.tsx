a6c5022a1ab59fb931ceccec414ff9d559607c0e
Edward Shin <pobredward@gmail.com>
2024-06-12 03:59:18 +0000

    Update ProductForm.tsx

--- components/ProductForm.tsx
+++ components/ProductForm.tsx
@@ -7,6 +7,7 @@ interface ProductFormProps {
   title?: string;
   description?: string;
   price?: string;
+  images?: string;
 }
 
 const ProductForm: React.FC<ProductFormProps> = ({
@@ -14,6 +15,7 @@ const ProductForm: React.FC<ProductFormProps> = ({
   title: initialTitle = "",
   description: initialDescription = "",
   price: initialPrice = "",
+  images,
 }) => {
   const [title, setTitle] = useState(initialTitle || "");
   const [description, setDescription] = useState(initialDescription || "");
@@ -38,6 +40,18 @@ const ProductForm: React.FC<ProductFormProps> = ({
     router.push("/products");
   }
 
+  const uploadImages = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (let i = 0; i < files.length; i++) {
+        data.append("file", files[i]);
+      }
+      const res = await axios.post("/api/upload", data);
+      console.log(res.data);
+    }
+  };
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
@@ -47,6 +61,28 @@ const ProductForm: React.FC<ProductFormProps> = ({
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
+      <label>Photos</label>
+      <div>
+        <label className="w-32 h-32 cursor-pointer flex flex-col justify-center items-center gap-1 text-sm text-gray-500 rounded-lg bg-gray-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImages} className="hidden"></input>
+        </label>
+        {!images?.length && <div>No photos in this product</div>}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="description"
