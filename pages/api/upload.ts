d621d41192524d544aa71d5b0622da0df1ff5d02
Edward Shin <pobredward@gmail.com>
2024-06-12 04:16:16 +0000

    Update upload.ts

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,13 +1,21 @@
-import multiparty from "multiparty";
+import multiparty, { Fields, File } from "multiparty";
+
+interface FormResult {
+  fields: Fields;
+  files: File[];
+}
 
 const handle = async (req, res) => {
   const form = new multiparty.Form();
-  form.parse(req, (err, fields, files) => {
-    if (err) throw err;
-    console.log('length:', files.length);
-    console.log(fields);
-    return res.json("ok");
+  const { fields, files } = await new Promise<FormResult>((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      resolve({ fields, files });
+    });
   });
+  console.log(files);
+  console.log(fields);
+  return res.json("ok");
 };
 
 export const config = {
