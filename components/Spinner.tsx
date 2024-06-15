7764d58926cf0773f58576ce408876751d28a597
Edward Shin <pobredward@gmail.com>
2024-06-15 02:30:38 +0000

    Create Spinner.tsx

---
+++ components/Spinner.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { PuffLoader } from "react-spinners";
+
+const Spinner: React.FC = () => {
+  return (
+    <PuffLoader color={'#000000'}/>
+  )
+}
+
+export default Spinner;
