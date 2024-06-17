9532e12051b685435d7679541aa1b8c7ae58a9f3
Edward Shin <pobredward@gmail.com>
2024-06-17 03:58:04 +0000

    Create categories.ts

---
+++ pages/api/categories.ts
@@ -0,0 +1,21 @@
+import mongooseConnect from "../../lib/mongoose";
+import Category from "../../models/Category";
+
+const handle = async (req, res) => {
+  const { method } = req;
+  await mongooseConnect();
+
+  if (method === "GET") {
+    res.json(await Category.find());
+  }
+
+  if (method === "POST") {
+    const { name } = req.body;
+    const categoryDoc = await Category.create({
+      name,
+    });
+    res.json(categoryDoc);
+  }
+};
+
+export default handle;
