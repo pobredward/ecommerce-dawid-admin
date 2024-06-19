5cfb021b75d0c1926cdd3eb2b75b6f8d093c7575
Edward Shin <pobredward@gmail.com>
2024-06-19 11:21:13 +0000

    Update Category.ts

--- models/Category.ts
+++ models/Category.ts
@@ -1,8 +1,14 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
+const PropertySchema = new Schema({
+  name: { type: String, required: true },
+  values: { type: String, required: true },
+});
+
 const CategorySchema = new Schema({
   name: { type: String, required: true },
   parent: { type: mongoose.Types.ObjectId, ref: "Category" },
+  properties: [PropertySchema],
 });
 
 const Category = models?.Category || model("Category", CategorySchema);
